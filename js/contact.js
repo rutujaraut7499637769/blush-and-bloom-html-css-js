4ae65d34be69bd6297a703ff9e942eddb1244597
Namrata Shekhar <shekharnamrata15@gmail.com>
2026-03-06 22:20:05 +0530

    contact page updated

--- js/contact.js
+++ js/contact.js
@@ -1,1 +1,22 @@
 alert("Welcome to blush & Bloom Contact Page.");
+
+let visitorCount = 0;
+visitorCount++;
+
+function sendMessage() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    let age = prompt("Enter your age");
+
+    age = parseInt(age);
+
+    alert(`Hello ${name}!
+    Thank you for contacting Blush & Bloom we received your message: ${message}
+    Your age: ${age} 
+     
+    Our team will contact you soon on ${email}`);
+
+
+}
